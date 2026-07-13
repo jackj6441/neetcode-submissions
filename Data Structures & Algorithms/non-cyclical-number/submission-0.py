db546c3c715484f3e792a36797de89b67a8f6c44
jackj6441 <164968702+jackj6441@users.noreply.github.com>
2026-07-13 00:20:47 -0500

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def getNum(n):
+            digits = [int(d) for d in str(n)]
+            res = 0
+            for d in digits:
+                res += d**2
+            print(n, res)
+            return res
+        m = {}
+        def dfs(n):
+            print(n)
+            if n in m:
+                return False
+            num = getNum(n)
+            if num == 1:
+                return True
+            else:
+                m[n] = True
+                return dfs(num)
+
+        return dfs(n)
