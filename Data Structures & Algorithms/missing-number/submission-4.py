852e7d58b1563ff35de04ef42c2750bf97cfbafa
jackj6441 <164968702+jackj6441@users.noreply.github.com>
2026-07-15 01:11:33 -0500

    Add: missing-number - submission-4

---
+++ Data Structures & Algorithms/missing-number/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        c = []
+        for i in range(len(nums)+1):
+            c.append(i)
+        # print(c)
+        for i in c:
+            if i not in nums:
+                return i
+        return 0
